b3afa5ed5c311d7cc2380687832cca082306c654
subash255 <suad_bca2077@lict.edu.np>
2025-05-13 15:24:04 +0545

    new file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->aliasMiddleware('rolemiddleware', \App\Http\Middleware\rolemiddleware::class);
+        $middleware->alias(['rolemiddleware' => \App\Http\Middleware\rolemiddleware::class]);
 
         
     })
